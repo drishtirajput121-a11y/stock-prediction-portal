1948da3990d8eba04e82e9199c537f930bc7b68c
drishtirajput121-a11y <drishtirajput121@gmail.com>
2026-05-13 11:38:37 +0530

    homepage react

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <>
+   <footer className='footer py-3 my-3 text-center'>
+    <hr className='border-bottom' />
+    <p className='text-light'>© 2026 Build with ❤ by Drishti Rajput</p>
+    </footer>
+   </>
+  )
+}
+
+export default Footer
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+     <>
+    <div className='container'>
+        <div className='p-5 textg center bg-light-dark rounded align-items-center mt-5'>
+            <h1 className='text-light'>Stock Prediction Portal</h1>
+            <p className='text-light lead'>Predicting the future of stocks with precision</p>
+            <Button text="Register" class = "btn btn-info" />
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Main
